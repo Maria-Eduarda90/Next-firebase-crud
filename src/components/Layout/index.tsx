959edd50a339d22df055ef8d4413410d0b8f6a2a
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-12-11 13:35:01 -0300

    created component title and layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import { Title } from "../Title";
+
+export interface LayoutPros {
+    title: string;
+    children: ReactNode;
+}
+
+export function Layout({ title, children }: LayoutPros) {
+    return (
+        <div className={`
+            flex flex-col w-2/3
+            bg-white text-gray-800
+        `}>
+            <Title>{title}</Title>
+            <div>
+                {children}
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Title/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+export interface TitleProps {
+    children: ReactNode;
+}
+
+export function Title({ children }: TitleProps) {
+    return (
+        <div className="flex flex-col justify-center">
+            <h1 className="px-5 py-2 text-2xl">
+                {children}
+            </h1>
+            <hr className="border-2 border-purple-500" />
+        </div>
+    );
+}
