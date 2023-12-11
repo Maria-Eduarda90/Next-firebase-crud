00dad1a3ad56ef77c7ba364435a6c3c51845db57
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-12-11 13:36:37 -0300

    chore: importing the layout component on my main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Layout } from "@/components/Layout";
+
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <div className="
+      flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-700 text-white
+    ">
+      <Layout title="Cadastro">
+        <span>ksjdhkjhsdf</span>
+      </Layout>
+    </div>
   )
 }
