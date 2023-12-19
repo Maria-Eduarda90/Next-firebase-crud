49f043ad1017ebeb02937a15bb5fa880193ed2ac
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2023-12-18 23:10:29 -0300

    chore: calling my hooks to use on my page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,47 +4,26 @@ import { Button } from "@/components/Button";
 import { Form } from "@/components/Form";
 import { Layout } from "@/components/Layout";
 import { Table } from "@/components/Table";
-import { Client } from "@/core/Client";
-import { useVisible } from "@/hook/useVisible";
-import { useState } from "react";
+import { useClient } from "@/hooks/useClient";
 
 export default function Home() {
-  const [client, setClient] = useState<Client>(Client.empty);
-  const { visible, setVisible } = useVisible();
-
-  const customers = [
-    new Client('Ane', 17, '1'),
-    new Client('Ian', 20, '2'),
-    new Client('Carla', 30, '3'),
-    new Client('Gustavo', 18, '4'),
-    new Client('Paulo', 24, '5')
-  ];
-
-  function clientSelected(client: Client) {
-    setClient(client);
-    setVisible('form');
-  };
-
-  function clientDeleted(client: Client) {
-    console.log('excluido: ', client.getName)
-  }
-
-  function saveCustomer(client: Client) {
-    console.log('salvo: ', client);
-    setVisible('table')
-  }
-
-  function newClient() {
-    setClient(Client.empty());
-    setVisible('form')
-  }
+  const {
+    client,
+    customers,
+    clientSelected,
+    clientDeleted,
+    newClient,
+    saveCustomer,
+    visibleTable,
+    displayTable
+  } = useClient();
 
   return (
     <div className="
       flex justify-center items-center h-screen bg-gradient-to-r from-teal-600 to-teal-800 text-white
     ">
       <Layout title="CRUD - FIREBASE">
-        {visible === 'table' ? (
+        {visibleTable ? (
           <div>
             <div className="flex justify-end">
               <Button
@@ -65,7 +44,7 @@ export default function Home() {
           <Form
             client={client}
             customerChanged={saveCustomer}
-            canceled={() => setVisible('table')}
+            canceled={displayTable}
           />
         )}
       </Layout>
